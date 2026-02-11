8c427743840cafbc2b6a6c34a4d2eb7ec09f1830
MariaEmilia611 <maria945duarteferreira@outlook.com>
2026-02-11 15:47:13 -0300

    Task.1

---
+++ src/components/TaskList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="task-list-container">
+        <h2>Lista de Tarefas</h2>
+
+        <div class="controls">
+            <button class="btn-add">Adicionar Nova Tarefa</button>
+            <button class="btn-toggle-all">
+                Marcar Todas
+            </button>
+            <button class="btn-clear">
+                Limpar Concluidas
+            </button>
+        </div>
+
+        <div class="tasks-coitainer">
+            <div class="peding-tasks">
+                <h3>Tarefas Pendentes</h3>
+                <span>Lista de Tarefas Pendentes</span>
+            </div>
+
+            <div class="completed-tasks">
+                <h3>Tarefas Concluidas</h3>
+                <span>Lista de Tarefas Concluidas</span>
+            </div>
+
+        </div>
+    </div>
+</template>
